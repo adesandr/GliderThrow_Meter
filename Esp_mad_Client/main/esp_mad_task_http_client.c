09e80e4c5bd0180781e4e070a2091885b31181e7
adesandr <32787979+adesandr@users.noreply.github.com>
2019-01-03 13:29:51 +0100

    Add files via upload

---
+++ Esp_mad_Client/main/esp_mad_task_http_client.c
@@ -0,0 +1,284 @@
+/**
+ * \file    esp_mad_task_http_client.c
+ * \author  Alain Désandré
+ * \version 1.0
+ * \date    November 18th 2018
+ * \brief   This file include all the code to deal with a simple http server.
+ *
+ * \details HTTP server deal with the various http request from the client.
+ *			At the fist reception of an http get from one client, the server
+ *			respond with the esp_mad HTML page (see esp_html.h).
+ *          
+ */
+
+/*--- All the include needed ---*/
+#include <esp_wifi.h>
+#include <esp_event_loop.h>
+#include <esp_log.h>
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include <sys/param.h>
+#include <freertos/event_groups.h>
+#include <esp_http_client.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define ESP_MAD_WIFI_SSID "ESP_MAD"
+
+extern float angle;
+extern bool BInit;
+
+/* FreeRTOS event group to signal when we are connected & ready to make a request */
+
+static EventGroupHandle_t wifi_event_group;
+
+/* The event group allows multiple bits for each event,
+
+   but we only care about one event - are we connected
+
+   to the AP with an IP? */
+
+const int CONNECTED_BIT = BIT0;
+
+static const char *TAG="Esp_Client";
+
+/**
+ *	\fn 	    esp_err_t event_handler(void *ctx, system_event_t *event).
+ *	\brief 		task launch the function to initialize wize .
+ *	\param[in]	*ctx : httpd_handler_t pointer.
+ *	\param[in]	*event : system_event_t event pointer.
+ *	\return		ESP_OK
+ */
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+
+    switch(event->event_id) 
+	{
+
+    	case SYSTEM_EVENT_STA_START:
+
+        	ESP_LOGI(TAG, "SYSTEM_EVENT_STA_START");
+
+        	ESP_ERROR_CHECK(esp_wifi_connect());
+
+        	break;
+
+    	case SYSTEM_EVENT_STA_GOT_IP:
+        
+            xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+
+        	ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
+
+        	ESP_LOGI(TAG, "Got IP: '%s'",
+
+            ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
+
+        	break;
+
+    	case SYSTEM_EVENT_STA_DISCONNECTED:
+
+        	ESP_LOGI(TAG, "SYSTEM_EVENT_STA_DISCONNECTED");
+            
+            /* This is a workaround as ESP32 WiFi libs don't currently
+
+               auto-reassociate. */
+
+        	ESP_ERROR_CHECK(esp_wifi_connect());
+
+            xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+
+    	    break;
+
+	    default:
+
+    	    break;
+
+    } /* edn switch */	
+
+    return ESP_OK;
+
+} /* end event_handler */
+
+
+/**
+ *	\fn 	    static void initialise_wifi(void *arg);
+ *	\brief 		wifi initialisation.
+ *	\param[in]	void*
+ *	\return		void.
+ */
+static void initialise_wifi(void *arg)
+{
+
+    tcpip_adapter_init();
+
+    wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+
+    wifi_config_t wifi_config = {
+
+        .sta = {
+
+            .ssid = ESP_MAD_WIFI_SSID,
+
+        },
+
+    };
+
+    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+} /* end initialise wifi */
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+
+{
+
+    switch(evt->event_id) {
+
+        case HTTP_EVENT_ERROR:
+
+            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+
+            break;
+
+        case HTTP_EVENT_ON_CONNECTED:
+
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+
+            break;
+
+        case HTTP_EVENT_HEADER_SENT:
+
+            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+
+            break;
+
+        case HTTP_EVENT_ON_HEADER:
+
+
+            break;
+
+        case HTTP_EVENT_ON_DATA:
+
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+
+                // Write out data
+
+                // printf("%.*s", evt->data_len, (char*)evt->data);
+
+            }
+
+            break;
+
+        case HTTP_EVENT_ON_FINISH:
+
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+
+            break;
+
+        case HTTP_EVENT_DISCONNECTED:
+
+            ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
+
+            break;
+
+    }
+
+    return ESP_OK;
+
+} /* end _http_event_handler() */
+
+/**
+ *	\fn 	    task_http_server.
+ *	\brief 		task launch the function to initialize wize .
+ *	\param[in]	void*
+ *	\return		void.
+ */
+void task_http_client(void *ignore)
+
+{
+    char post_data[20];
+
+    EventBits_t uxBits;
+
+    esp_err_t ret = nvs_flash_init();
+    
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+
+      ESP_ERROR_CHECK(nvs_flash_erase());
+
+      ret = nvs_flash_init();
+    }
+
+    ESP_ERROR_CHECK(ret);
+
+    initialise_wifi(NULL);
+
+	while(1)
+	{
+
+    uxBits = xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);    
+
+    /*--- if station connected and MPU calibration done ---*/
+    if(((uxBits & CONNECTED_BIT) != 0) && BInit)
+        {
+
+        esp_http_client_config_t config = {
+
+            .url = "http://192.168.1.1/sensor2",
+
+            .event_handler = _http_event_handler,
+
+            };
+
+        esp_http_client_handle_t client = esp_http_client_init(&config);
+
+        sprintf(post_data,"%.1f", angle);
+
+        esp_http_client_set_url(client, "http://192.168.1.1/sensor2");
+
+        esp_http_client_set_method(client, HTTP_METHOD_POST);
+
+        esp_http_client_set_post_field(client, post_data, strlen(post_data));
+
+        esp_err_t err = esp_http_client_perform(client);
+
+        if (err == ESP_OK) {
+
+            ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %d",
+
+                esp_http_client_get_status_code(client),
+
+                esp_http_client_get_content_length(client));
+
+            } else {
+
+            ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+            }
+
+        esp_http_client_cleanup(client);
+
+
+        } /* end if uxBits*/  
+
+    vTaskDelay(900/portTICK_PERIOD_MS);
+	
+    } /* end while() */
+
+    vTaskDelete(NULL);
+
+} /* end task_http_client() */
---
+++ Esp_mad_Client/main/esp_mad_task_measure.cpp
@@ -0,0 +1,244 @@
+/**
+ * \file      esp_mad_task_measure.cpp
+ * \author    Alain Désandré
+ * \version   1.0
+ * \date      28 octobre 2018
+ * \brief     This task process the measure and calibration of the MPU6050
+ *
+ * \details   This task handles the periodicall MPU6050 measurement and computes the 
+ *            angle and travel of the wing surface. This file is part of the esp_mad
+ *            project.
+ */
+
+#include <esp_log.h>
+#include <esp_err.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "mpu6050.h"
+#include "I2Cdev.h"
+#include "driver/i2c.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "sdkconfig.h"
+#include "math.h"
+
+#define PI 3.14116
+#define PIN_SDA 23
+#define PIN_CLK 19
+
+int chordControlSurface = 50;             // Chord in mm. 50 mm by default.
+
+int16_t ax, ay, az;                       // raw measure
+int16_t gx, gy, gz;
+uint8_t Accel_range;
+uint8_t Gyro_range;
+float travel = 0.0;
+float angle=0.0;
+
+bool BInit = 0;
+
+//Change this 3 variables if you want to fine tune the skecth to your needs.
+int buffersize=1000;     //Amount of readings used to average, make it higher to get more precision but sketch will be slower  (default:1000)
+int acel_deadzone=8;     //Acelerometer error allowed, make it lower to get more precision, but sketch may not converge  (default:8)
+int giro_deadzone=1;     //Giro error allowed, make it lower to get more precision, but sketch may not converge  (default:1)
+
+int mean_ax,mean_ay,mean_az,mean_gx,mean_gy,mean_gz,state=0;
+int ax_offset,ay_offset,az_offset,gx_offset,gy_offset,gz_offset;
+
+MPU6050 mpu = MPU6050();
+
+/**
+ * 	/fn			void meansensors(void)
+ *	/brief		average sensors reading 
+ * 	/param[in]	void
+ *	/return		void
+ */
+void meansensors(void){
+  long i=0,buff_ax=0,buff_ay=0,buff_az=0,buff_gx=0,buff_gy=0,buff_gz=0;
+
+  while (i<(buffersize+101)){
+    // read raw accel/gyro measurements from device
+    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    if (i>100 && i<=(buffersize+100)){ //First 100 measures are discarded
+      buff_ax=buff_ax+ax;
+      buff_ay=buff_ay+ay;
+      buff_az=buff_az+az;
+      buff_gx=buff_gx+gx;
+      buff_gy=buff_gy+gy;
+      buff_gz=buff_gz+gz;
+    }
+    if (i==(buffersize+100)){
+      mean_ax=buff_ax/buffersize;
+      mean_ay=buff_ay/buffersize;
+      mean_az=buff_az/buffersize;
+      mean_gx=buff_gx/buffersize;
+      mean_gy=buff_gy/buffersize;
+      mean_gz=buff_gz/buffersize;
+    }
+    i++;
+    vTaskDelay(2/portTICK_PERIOD_MS); //Needed so we don't get repeated measures
+  }
+} /* End meansensors() */
+
+/**
+ *	/fn 		void calibration(void)
+ *	/brief		MPU6050 calibration
+ * 	/param[in]	void
+ *	/return		void
+ *
+ */
+void calibration(void){
+  ax_offset=-mean_ax/8;
+  ay_offset=-mean_ay/8;
+  az_offset=(16384-mean_az)/8;
+
+  gx_offset=-mean_gx/4;
+  gy_offset=-mean_gy/4;
+  gz_offset=-mean_gz/4;
+  
+  while (1){
+    int ready=0;
+    mpu.setXAccelOffset(ax_offset);
+    mpu.setYAccelOffset(ay_offset);
+    mpu.setZAccelOffset(az_offset);
+
+    mpu.setXGyroOffset(gx_offset);
+    mpu.setYGyroOffset(gy_offset);
+    mpu.setZGyroOffset(gz_offset);
+
+    meansensors();
+
+    if (abs(mean_ax)<=acel_deadzone) ready++;
+    else ax_offset=ax_offset-mean_ax/acel_deadzone;
+
+    if (abs(mean_ay)<=acel_deadzone) ready++;
+    else ay_offset=ay_offset-mean_ay/acel_deadzone;
+
+    if (abs(16384-mean_az)<=acel_deadzone) ready++;
+    else az_offset=az_offset+(16384-mean_az)/acel_deadzone;
+
+    if (abs(mean_gx)<=giro_deadzone) ready++;
+    else gx_offset=gx_offset-mean_gx/(giro_deadzone+1);
+
+    if (abs(mean_gy)<=giro_deadzone) ready++;
+    else gy_offset=gy_offset-mean_gy/(giro_deadzone+1);
+
+    if (abs(mean_gz)<=giro_deadzone) ready++;
+    else gz_offset=gz_offset-mean_gz/(giro_deadzone+1);
+
+    if (ready==6) break;
+  }
+} /* End calibration */
+
+/**
+ *	/fn 		void InitMPU6050(void)
+ *  /brief		MPU6050 Initialisation	
+ *	/param[in]	void
+ *	/return		void	
+ * 
+ */
+void InitMPU6050(void)
+{
+	static const char tagI[] = "Init ->";
+
+	/*--- Display message ---*/
+  	ESP_LOGI(tagI, "Calibration Start...\n");
+
+    /*--- reset offsets   ---*/
+  	mpu.setXAccelOffset(0);
+  	mpu.setYAccelOffset(0);
+  	mpu.setZAccelOffset(0);
+  	mpu.setXGyroOffset(0);
+    mpu.setYGyroOffset(0);
+	mpu.setZGyroOffset(0);
+
+  	/*--- Step 1 : first reading ---*/
+  	ESP_LOGI(tagI,"First reading...\n");
+	meansensors();
+  	vTaskDelay(100/portTICK_PERIOD_MS);
+
+  	/*--- Step 2 : Compute offsets ---*/
+  	ESP_LOGI(tagI,"Compute offsets...\n");
+	calibration();
+  	vTaskDelay(100/portTICK_PERIOD_MS);
+
+  	/*--- New reads and offsets display ---*/
+  	meansensors();
+	ESP_LOGI(tagI, "FINISHED!\n");
+	ESP_LOGI(tagI, "offset: ax: %d - ay: %d - az: %d - gx: %d - gy: %d - gz: %d\n",
+			ax_offset, ay_offset, az_offset,gx_offset,gy_offset,gz_offset);
+	ESP_LOGI(tagI, "Read w. off : ax : %d - ay :%d - az :%d - gx : %d - gy :%d - gz :%d\n",
+			mean_ax,mean_ay,mean_az,mean_gx,mean_gy,mean_gz);
+
+	BInit = 1;	
+
+  	/*--- Set offsets with the compute values ---*/
+  	mpu.setXAccelOffset(ax_offset);
+  	mpu.setYAccelOffset(ay_offset);
+  	mpu.setZAccelOffset(az_offset);
+  	mpu.setXGyroOffset(gx_offset);
+  	mpu.setYGyroOffset(gy_offset);
+  	mpu.setZGyroOffset(gz_offset);
+  
+} /* End Init() */
+
+/**
+ *	/fn 		void task_measure(void*)
+ *  /brief		MPU6050 periodicall compute	
+ *	/param[in]	void*
+ *	/return		void	
+ * 
+ */
+void task_measure(void*){
+
+	static const char tagd[] = "task_measure->";
+
+    /*--- I2C Configuration and initialization ---*/
+	i2c_config_t conf;
+	conf.mode = I2C_MODE_MASTER;
+	conf.sda_io_num = (gpio_num_t)PIN_SDA;
+	conf.scl_io_num = (gpio_num_t)PIN_CLK;
+	conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+	conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+	conf.master.clk_speed = 400000;
+	ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &conf));
+	ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0));
+
+	/*--- MPU6050 Initialization ---*/
+	ESP_LOGI(tagd,"MPU6050 initialization ...");
+	mpu.initialize();
+
+	/*--- I2C Connexion Test ---*/
+	if(mpu.testConnection())
+		ESP_LOGI(tagd, "I2C connection OK\n");
+	
+	else
+	 	ESP_LOGI(tagd, "I2C connection Error\n");
+
+	/*--- MPU6050 Calibration ---*/
+	InitMPU6050();
+
+	/*--- Infinite loop ---*/
+	while(1){
+
+		/*--- Compute Y angle in degre. Complementary filter is used to combine accelero and gyro datas       ---*/
+    	/*--- see  http://www.pieter-jan.com/node/11 for more information regarding the complementary filter  ---*/
+    	/*--- or https://delta-iot.com/la-theorie-du-filtre-complementaire/ (in french)                       ---*/
+    	/*--- Basically complementary filter avoid used of kallman filter, quiet difficult to implement in    ---*/
+    	/*--- arduino platform. Gyro are used for fast motion as accelero are used for slow motion.           ---*/                                 
+    	mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    	angle=0.98*(angle+float(gy)*0.01/131) + 0.02*atan2((double)ax,(double)az)*180/PI;
+
+    	/*--- Compute Control surface travel using : 2* sin(angle/2)* chord. Angle for sinus function needs  ---*/
+    	/*--- to be converted in radian (angleDegre = angleRadian *(2*PI)/360)                             ---*/ 
+    	travel = chordControlSurface * sin((angle*(2.0*PI)/360.0)/2.0) * 2.0;
+
+	//	ESP_LOGI(tagd, "angle %f - travel %f\n",angle,travel);
+	//	ESP_LOGI(tagd, "(abs)angle %d - (abs)travel %d\n",(int)abs(angle), (int)abs(travel));
+
+		vTaskDelay(10/portTICK_PERIOD_MS);
+		
+		}
+
+	vTaskDelete(NULL);
+}
