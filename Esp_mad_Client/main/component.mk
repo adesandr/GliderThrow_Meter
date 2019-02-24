36f8f56d3aba81cf099986b69ff2a675668449ad
adesandr <alain.desandre@wanadoo.fr>
2019-02-24 21:56:57 +0100

    add Esp_mad.h

--- Esp_mad_Client/main/component.mk
+++ Esp_mad_Client/main/component.mk
@@ -4,3 +4,5 @@
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
 COMPONENT_ADD_INCLUDEDIRS=.
+
+COMPONENT_EXTRA_INCLUDES= C:/Alain/msys32/home/Robin/GliderThrow_Meter/Includes
